dbfa75fdc52f8c5d5aeb07714b61bfaf70c3b6ca
Ileok <apr_3@naver.com>
2021-09-30 02:02:32 +0900

    4주차 과제 [배열 뒤로 0값 이동]

--- week4/Array0.java
+++ week4/Array0.java
@@ -18,15 +18,12 @@ class Array0 {
 
         // 숫자 입력
         String input_ = in.nextLine();
-        // 숫자 사이의 공백 제거
-        String input = input_.replace(" ", "");
-        // int 배열 생성
-        int[] arr = new int[input.length()];
 
-        // 입력 받은 문자열 정수를 int 배열로 분할
-        for(int i=0; i<input.length(); i++) {
-            arr[i] = input.charAt(i) - '0';
-        }
+        // 공백을 기준으로 문자열 배열로 분리
+        String[] input = input_.split(" ");
+
+        // 문자열 배열을 정수형 배열로 변환
+        int[] arr = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
 
         reorder(arr); 
 
