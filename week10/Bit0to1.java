59ee74f33ade912318dc7d629e4854b69e6e58ad
Sodam <58940128+Ileok@users.noreply.github.com>
2021-11-09 09:10:18 +0900

    Create Bit0to1.java

---
+++ week10/Bit0to1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bit0to1 {
+	static int getPosOfRightmostSetBit(int n) {
+		return (int)((Math.log10(n & -n)) / (Math.log10(2))) + 1;
+	}
+		
+	static int setRightmostUnsetBit(int n) {
+		if (n == 0)
+			return 1;
+			
+		if ((n & (n + 1)) == 0)
+			return n;
+			
+		int pos = getPosOfRightmostSetBit(~n);
+		
+		return ((1 << (pos - 1)) | n);
+	}
+	        
+	public static void main(String arg[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("입력 : ");
+		int n = in.nextInt();
+		System.out.println("출력 : " + setRightmostUnsetBit(n));
+	}
+}
